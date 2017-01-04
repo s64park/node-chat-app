58ffb694898968dc9c5a15f1d95c3267d8849ab7
s64park <s64park@uwaterloo.ca>
2017-01-03 20:12:36 -0500

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -14,4 +14,18 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(msg) {
     console.log('newMessage', msg);
+    var li = jQuery('<li></li>');
+    li.text(`${msg.from}: ${msg.text}`);
+    jQuery('#messages').append(li);
 });
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    })
+})
